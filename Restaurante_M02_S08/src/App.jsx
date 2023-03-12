0cd6326224b40692d607be5253902fa3051fb9bc
DevThiagoSantana <113400543+DevThiagoSantana@users.noreply.github.com>
2023-03-12 13:28:18 -0300

    commit exercicio 02

--- Restaurante_M02_S08/src/App.jsx
+++ Restaurante_M02_S08/src/App.jsx
@@ -1,7 +1,12 @@
-import './App.css'
+import './App.css';
+import { Navbar } from './components/Navbar';
 
 function App() {
-  return <div></div> 
+  return (
+    <div className='app'>
+      <Navbar />
+    </div>
+  );
 }
 
-export default App
+export default App;
