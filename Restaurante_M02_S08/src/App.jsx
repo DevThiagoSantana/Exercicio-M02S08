3dde7752c6fbc7358a9b5cc7123e754a92b91a3d
DevThiagoSantana <113400543+DevThiagoSantana@users.noreply.github.com>
2023-03-12 14:01:33 -0300

    commit exercicio 04 card

--- Restaurante_M02_S08/src/App.jsx
+++ Restaurante_M02_S08/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { Footer } from './components/Footer/Footer';
 import { Navbar } from './components/Navbar';
+import { Card } from './components/Card';
 
 function App() {
   return (
@@ -8,6 +9,12 @@ function App() {
       <Navbar />
       <main className='main'>
         <h2>Produtos</h2>
+        <Card
+        imagem = "http://localhost:8081/assets/cordeiro.png"
+        nome = "Cordeiro Lajeana"
+        descricao = "Carré grelhado com delicado molho de três pimentas, batata suíça e banana caramelada."
+        valor = {169}
+        tempoPreparo = "50 min"/>
       </main>
 
       <Footer/>
