763ac40e535b3f57dfe6926df8eb1db5c939ab8c
DevThiagoSantana <113400543+DevThiagoSantana@users.noreply.github.com>
2023-03-12 15:57:31 -0300

    commit  exercicio 08

--- Restaurante_M02_S08/src/pages/Produtos/Produtos.jsx
+++ Restaurante_M02_S08/src/pages/Produtos/Produtos.jsx
@@ -1,10 +1,25 @@
-import './Produtos.css';
+import { useState } from 'react';
 import { Card } from '../../components/Card';
 import { useProdutos } from '../../hooks/useProdutos';
+import './Produtos.css';
 
-export const Produtos =() =>{
-  const{error,isLoading,produtos} = useProdutos();
-  
+export const Produtos = () => {
+  const { error, isLoading, produtos } = useProdutos();
+  const [selecionados, setSelecionados] = useState([]);
+
+  const isSelecionado = (id) => {
+    return selecionados.some((item) => item === id);
+  };
+
+  const handleSelecionar = (id) => {
+    if (isSelecionado(id)) {
+      setSelecionados((prev) => prev.filter((item) => item !== id));
+      return;
+    }
+
+    setSelecionados((prev) => [...prev, id]);
+  };
+
   if (isLoading) {
     return <p>Carregando...</p>;
   }
@@ -13,27 +28,32 @@ export const Produtos =() =>{
     return <p>{error}</p>;
   }
 
-  return(
-    <>    
-    {!!produtos &&
+  return (
+    <>
+      {!!produtos &&
         Object.keys(produtos).map((secao) => (
           <section key={secao} className='secao'>
             <h2 className='secao-titulo'>{secao}</h2>
 
+            <hr />
+
             <div className='lista'>
-            {produtos[secao].map((item) => (
+              {produtos[secao].map((item) => (
                 <Card
-                key={item.id}
-                imagem={item.img}
-                nome={item.nome}
-                descricao={item.descricao}
-                valor={item.valor}
-                tempoPreparo={item.tempoPreparo}
+                  key={item.id}
+                  id={item.id}
+                  imagem={item.img}
+                  nome={item.nome}
+                  descricao={item.descricao}
+                  valor={item.valor}
+                  tempoPreparo={item.tempoPreparo}
+                  selecionado={isSelecionado(item.id)}
+                  onSelecionar={handleSelecionar}
                 />
-            ))}
-          </div>
-      </section>
-      ))}
-      </>
+              ))}
+            </div>
+          </section>
+        ))}
+    </>
   );
 };
